311a5cebac5f9b1501a3b9a919393343c6164b08
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2019-04-16 07:07:37 +0530

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,428 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Shaily Shrivastava </title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+       <!-- STYLES -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/plugins.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<!--[if lt IE 9]> <script type="text/javascript" src="js/modernizr.custom.js"></script> <![endif]-->
+<!-- /STYLES -->
+
+    </head>
+    <body>
+        <!-- WRAPPER ALL -->
+<div class="edrea_tm_wrapper_all">
+
+	<div id="edrea_tm_popup_blog">
+		<div class="container">
+			<div class="inner_popup scrollable"></div>
+		</div>
+		<span class="close"><a href="#"></a></span>
+	</div>
+	
+	<!-- PRELOADER -->
+	<div class="edrea_tm_preloader">
+		<div class="spinner_wrap">
+			<div class="spinner"></div>
+		</div>
+	</div>
+	<!-- /PRELOADER -->
+	
+    <!-- HEADER -->
+    <header class="edrea_tm_header">
+		<div class="edrea_tm_navigation_wrap">
+			<div class="container">
+				<div class="navigation_inner"> 
+					<div class="logo">
+						<a class="light_logo" href="index.html">
+                            <img src="https://res.cloudinary.com/dazzqcenn/image/upload/v1555375153/shaily.jpg" alt="" width="200px">
+                        </a>
+					</div>
+					<div class="nav_list_wrap">
+						<div class="menu">
+							<ul class="anchor_nav">
+								<li><a href="#home">Home</a></li>
+								<li><a href="#about">About</a></li>
+								<li><a href="#services">Services</a></li>
+								<li><a href="#portfolio">Portfolio</a></li>
+								<li><a href="#news">News</a></li>
+								<li><a href="#contact">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="edrea_tm_trigger">
+						<div class="hamburger hamburger--collapse-r">
+							<div class="hamburger-box">
+								<div class="hamburger-inner"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+   		<div class="edrea_tm_mobile_menu_wrap">
+   			<div class="mob_menu">
+				<ul class="anchor_nav">
+					<li><a href="#home">Home</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#services">Services</a></li>
+					<li><a href="#portfolio">Portfolio</a></li>
+					<li><a href="#news">News</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+    </header>
+    <!-- /HEADER -->
+    
+    <!-- CONTENT -->
+	<div class="edrea_tm_content">
+	
+		<!-- HERO -->
+		<div class="edrea_tm_section" id="home">
+			<div class="edrea_tm_hero_header">
+				<div class="edrea_tm_universal_box_wrap">
+					<div class="bg_wrap">
+						<div class="overlay_image hero jarallax" data-speed="0"></div>
+						<div class="overlay_video"></div>
+						<div class="overlay_color hero"></div>
+					</div>
+					<div class="content hero">
+						<div class="container hero">
+							<div class="edrea_tm_hero_title">
+								<p class="animate"><span class="another"></span> <span class="edrea_tm_animation_text_word"></span></p>
+								<div class="edrea_tm_hero_share">
+									<ul>
+										<li><a href="#"><i class="xcon-facebook"></i></a></li>
+										<li><a href="#"><i class="xcon-twitter"></i></a></li>
+										<li><a href="#"><i class="xcon-linkedin"></i></a></li>
+										<li><a href="#"><i class="xcon-instagram"></i></a></li>
+										<li><a href="#"><i class="xcon-behance"></i></a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="edrea_tm_arrow_wrap bounce anchor">
+								<a href="#about"><i class="xcon-angle-double-down"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="svg shape">
+					<svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
+						<polygon points="-30,300 355.167,210.5 1432.5,290 1920,198.5 1920,300"></polygon>
+					</svg>
+				</div>
+			</div>
+		</div>
+		<!-- /HERO -->
+
+		<!-- ABOUT -->
+		<div class="edrea_tm_section relative" id="about">
+			<div class="edrea_tm_about_wrapper_all">
+				<div class="container">
+					<div class="edrea_tm_about_wrap">
+						<div class="author_wrap">
+							<div class="leftbox">
+								<div class="about_image_wrap parallax" data-relative-input="true">
+									<div class="image layer" data-depth="0.1">
+										<img src="s5.jpg" alt="" width="400px" />
+										<div class="inner"></div>
+									</div>
+									<div class="border layer" data-depth="0.2">
+										<img src="s5.jpg" alt="" width="400px" />
+										<div class="inner"></div>
+									</div>
+								</div>
+								
+							</div>
+							<div class="rightbox">
+								<div class="edrea_tm_main_title_holder">
+									<h3><span>About Me</span></h3>
+								</div>
+								<div class="definition">
+                                    <p>Hi Guys! i am  <strong>Shaily Shrivastava</strong>. I use people to get famous as a singer ,
+                                         i have fooled many and i have been doing this since 1 year now. With 1 year of experience i have
+                                          fooled many guys and become famous using them and then i throw them away. i have a boyfriend who is 
+                                          very far away so i spend time doing time pass with other boys.
+                                    </p>
+								</div>
+								<div class="progress_bar_wrap_total">
+									<div class="edrea_tm_progress_wrap" data-size="small" data-round="c" data-strip="off">
+										<div class="edrea_tm_progress" data-value="95" data-color="#000">
+											<span><span class="label">Singing Skills</span><span class="number">95%</span></span>
+											<div class="edrea_tm_bar_bg"><div class="edrea_tm_bar_wrap"><div class="edrea_tm_bar"></div></div></div>
+										</div>
+										<div class="edrea_tm_progress" data-value="85" data-color="#000">
+											<span><span class="label">Being a Bitch</span><span class="number">85%</span></span>
+											<div class="edrea_tm_bar_bg"><div class="edrea_tm_bar_wrap"><div class="edrea_tm_bar"></div></div></div>
+										</div>
+										<div class="edrea_tm_progress" data-value="75" data-color="#000">
+											<span><span class="label">Playing with Boys Hearts</span><span class="number">75%</span></span>
+											<div class="edrea_tm_bar_bg"><div class="edrea_tm_bar_wrap"><div class="edrea_tm_bar"></div></div></div>
+										</div>
+									</div>
+								</div>
+								<div class="about_short_contact_wrap">
+									<ul>
+										<li>
+											<span><label>Phone:</label> +77 022 177 05 05</span>
+										</li>
+										<li>
+											<span><label>Mail:</label> <a href="index.html">example&#64;gmail.com</a></span>
+										</li>
+									</ul>
+								</div>
+								<div class="buttons_wrap">
+									<ul>
+										<li>
+											<a href="index.html"><span>Download CV</span></a>
+										</li>
+										<li class="anchor">
+											<a href="#contact"><span>Send Message</span></a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="svg shape another">
+					<svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
+						<polygon points="-30,300 355.167,210.5 1432.5,290 1920,198.5 1920,300"></polygon>
+					</svg>
+				</div>
+			</div>
+		</div>
+		<!-- /ABOUT -->
+		<!-- SERVICES -->
+		<div class="edrea_tm_section relative" id="services">
+			<div class="edrea_tm_service_wrapper_all">
+				<div class="container">
+					<div class="edrea_tm_main_title_holder">
+						<h3><span>Amazing Services</span></h3>
+					</div>
+					<div class="edrea_tm_services_wrap">
+						<div class="carousel_nav">
+							<div class="custom_nav">
+								<a href="#" class="prev"></a>
+								<a href="#" class="next"></a>
+							</div>
+						</div>
+						<ul class="owl-carousel">
+							<li class="item">
+								<div class="inner">
+									<div class="overlay"></div>
+									<div class="texts_wrap">
+										<h3>Singing & Bitching</h3>
+										<p>During this phase, we will work to provide a detailed analysis of the project and we will establish project expectations along with our clients.</p>
+									</div>
+									<span class="arrow"><img class="svg" src="img/svg/arrow-right.svg" alt="" /></span>
+									<span class="shape">
+										<img class="svg" src="img/svg/layers.svg" alt="" />
+										<span></span>
+									</span>
+								</div>
+							</li>
+							<li class="item">
+								<div class="inner">
+									<div class="overlay"></div>
+									<div class="texts_wrap">
+										<h3>Flirting</h3>
+										<p>The client retains an architect or engineer to design the project and to prepare the necessary drawings and specifications for new project.</p>
+									</div>
+									<span class="arrow"><img class="svg" src="img/svg/arrow-right.svg" alt="" /></span>
+									<span class="shape">
+										<img class="svg" src="img/svg/photo-camera.svg" alt="" />
+										<span></span>
+									</span>
+								</div>
+							</li>
+							<li class="item">
+								<div class="inner">
+									<div class="overlay"></div>
+									<div class="texts_wrap">
+										<h3>Playing with Boys Hearts</h3>
+										<p>Under a Construction Management contract, the client secures the services of a construction manager to work with the design team.</p>
+									</div>
+									<span class="arrow"><img class="svg" src="img/svg/arrow-right.svg" alt="" /></span>
+									<span class="shape">
+										<img class="svg" src="img/svg/share.svg" alt="" />
+										<span></span>
+									</span>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="svg shape">
+				<svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
+					<polygon points="-30,300 355.167,210.5 1432.5,290 1920,198.5 1920,300"></polygon>
+				</svg>
+			</div>
+		</div>
+		<!-- /SERVICES -->
+		
+		<!-- PORTFOLIO -->
+		<div class="edrea_tm_section relative" id="portfolio">
+			<div class="edrea_tm_portfolio_wrapper_all" data-portfolio-style="filterable">
+				
+				<!-- PORTFOLIO FILTER -->
+				<div class="edrea_tm_second_portfolio">
+				<div class="container">
+					<div class="edrea_tm_portfolio_wrap">
+						<div class="edrea_tm_main_title_holder portfolio">
+							<h3><span>Awesome Works</span></h3>
+						</div>
+						<div class="edrea_tm_portfolio_titles"></div>
+						<ul class="edrea_tm_portfolio_filter">
+							<li><a href="#" class="current" data-filter="*">All</a></li>
+							<li><a href="#" data-filter=".design">Bitch Look</a></li>
+							<li><a href="#" data-filter=".photography">Ego</a></li>
+							<li><a href="#" data-filter=".development">Playing Around</a></li>
+						</ul>
+						<ul class="edrea_tm_portfolio_list gallery_zoom">
+							<li class="design">
+								<div class="entry edrea_tm_portfolio_animation_wrap" data-title="Aoc Productions" data-category="Design">
+									<a class="zoom" href="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376754/s4.jpg">
+										<img src="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376754/s4.jpg" alt="" />
+										<div class="edrea_tm_portfolio_image_main"></div>
+									</a>
+								</div>
+							</li>
+							<li class="photography">
+								<div class="entry edrea_tm_portfolio_animation_wrap" data-title="Ind Hed" data-category="Photography">
+									<a class="zoom" href="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376737/s3.jpg">
+										<img src="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376737/s3.jpg" alt="" />
+										<div class="edrea_tm_portfolio_image_main"></div>
+									</a>
+								</div>
+							</li>
+							<li class="development">
+								<div class="entry edrea_tm_portfolio_animation_wrap" data-title="Paper Mockup" data-category="Development">
+									<a class="zoom" href="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376737/s3.jpg">
+										<img src="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376737/s3.jpg" alt="" />
+										<div class="edrea_tm_portfolio_image_main"></div>
+									</a>
+								</div>
+							</li>
+							<li class="photography">
+								<div class="entry edrea_tm_portfolio_animation_wrap" data-title="The Nordic" data-category="Photography">
+									<a class="zoom" href="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376734/s6.jpg">
+										<img src="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376734/s6.jpg" alt="" />
+										<div class="edrea_tm_portfolio_image_main"></div>
+									</a>
+								</div>
+							</li>
+							<li class="design">
+								<div class="entry edrea_tm_portfolio_animation_wrap" data-title="Creatives Castle" data-category="Design">
+									<a class="zoom" href="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376737/s7.jpg">
+										<img src="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376737/s7.jpg" alt="" />
+										<div class="edrea_tm_portfolio_image_main"></div>
+									</a>
+								</div>
+							</li>
+							<li class="photography">
+								<div class="entry edrea_tm_portfolio_animation_wrap" data-title="White Bag" data-category="Photography">
+									<a class="zoom" href="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376745/s8.jpg">
+										<img src="https://res.cloudinary.com/dazzqcenn/image/upload/v1555376745/s8.jpg" alt="" />
+										<div class="edrea_tm_portfolio_image_main"></div>
+									</a>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- /PORTFOLIO FILTER -->
+			
+			</div>
+			<div class="svg shape another">
+				<svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
+					<polygon points="-30,300 355.167,210.5 1432.5,290 1920,198.5 1920,300"></polygon>
+				</svg>
+			</div>
+		</div>
+		<!-- /PORTFOLIO -->
+		
+		<!-- NEWS -->
+		<!-- /NEWS -->
+		
+	</div>
+	<!-- /CONTENT -->
+	
+	<!-- CONTACT & FOOTER -->
+	<div class="edrea_tm_section" id="contact">
+		<div class="edrea_tm_footer_contact_wrapper_all">
+			<div class="edrea_tm_contact_wrap_all">
+				<div class="container">
+					<div class="leftbox">
+						<div class="edrea_tm_main_title_holder contact">
+							<h3><span>Get in Touch</span></h3>
+						</div>
+						<div class="short_info_wrap">
+							<ul>
+								<li><p><label>Address:</label><span>Nerul</span></p></li>
+								<li><p><label>Email:</label><span><a href="#">shaily</a></span></p></li>
+								<li><p><label>Phone:</label><span>+‭917899210888‬</span></p></li>
+							</ul>
+						</div>
+					</div>
+					<div class="rightbox">
+						<div class="edrea_tm_contact_wrap">
+							<div class="main_input_wrap">
+								<form action="/" method="post" class="contact_form" id="contact_form">
+									<div class="returnmessage" data-success="Your message has been received, We will contact you soon."></div>
+									<div class="empty_notice"><span>Please Fill Required Fields</span></div>
+									<div class="wrap">
+										<input id="name" type="text" placeholder="Your Name">
+									</div>
+									<div class="wrap">
+										<input id="email" type="text" placeholder="Your Email">
+									</div>
+									<div class="wrap">
+										<textarea id="message" placeholder="Your Message"></textarea>
+									</div>
+									<div class="edrea_tm_button">
+										<a id="send_message" href="#"><span>Send Message</span></a>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="edrea_tm_footer_total_wrap">
+				<div class="edrea_tm_footer_wrap">
+					<div class="container">
+						<p>&copy; Copyright 2018.Shaily Shrivastava.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- /FCONTACT & FOOTER -->
+ 	
+ 	 <a class="edrea_tm_totop" href="#"></a> 
+ 	 
+</div>
+<!-- / WRAPPER ALL -->
+	
+<!-- SCRIPTS -->
+<script src="js/jquery.js"></script>
+<!--[if lt IE 10]> <script type="text/javascript" src="js/ie8.js"></script> <![endif]-->	
+<script src="js/plugins.js"></script>
+<script src="js/init.js"></script>
+<!-- /SCRIPTS -->
+    </body>
+</html>
